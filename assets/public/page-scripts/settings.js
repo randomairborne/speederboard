0d2a3978f75f35088ace1de2f0e73a85204d2fbb
valkyrie_pilot <valk@randomairborne.dev>
2023-10-25 19:45:52 -0600

    fix js settings for real

--- assets/public/page-scripts/settings.js
+++ assets/public/page-scripts/settings.js
@@ -1,11 +1,25 @@
-const pfp_picker = document.getElementById("pfp");
-const pfp_form = document.getElementById("pfp-form");
-const banner_picker = document.getElementById("banner");
-const banner_form = document.getElementById("banner-form");
-const stylesheet_picker = document.getElementById("stylesheet");
-const stylesheet_form = document.getElementById("css-form");
+function addOnSubmits() {
+    const pfp_picker = document.getElementById("pfp");
+    const pfp_form = document.getElementById("pfp-form");
+    const banner_picker = document.getElementById("banner");
+    const banner_form = document.getElementById("banner-form");
+    const stylesheet_picker = document.getElementById("stylesheet");
+    const stylesheet_form = document.getElementById("css-form");
 
-pfp_picker.addEventListener("change", pfp_form.submit);
-banner_picker.addEventListener("change", banner_form.submit);
-stylesheet_picker.addEventListener("change", stylesheet_form.submit);
+    pfp_picker.addEventListener("change", function (_) {
+        console.log("Submitting pfp form")
+        pfp_form.submit();
+    });
+    banner_picker.addEventListener("change", function (_) {
+        console.log("Submitting banner form")
+        banner_form.submit();
+    });
+    stylesheet_picker.addEventListener("change", function (_) {
+        console.log("Submitting stylesheet form")
+        stylesheet_form.submit();
+    });
+}
 
+document.addEventListener('DOMContentLoaded', function() {
+    addOnSubmits();
+});
