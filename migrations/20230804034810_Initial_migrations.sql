33f0f6b40e3b8c78fc84f06217614b2beef8b250
valkyrie_pilot <valk@randomairborne.dev>
2023-11-14 22:14:39 -0700

    INDEXES

--- migrations/20230804034810_Initial_migrations.sql
+++ migrations/20230804034810_Initial_migrations.sql
@@ -15,9 +15,9 @@ CREATE TABLE users (
     language VARCHAR(5)
 );
 
-CREATE INDEX users_name_index ON users(username);
-CREATE UNIQUE INDEX case_insensitive_name_index ON users(lower(username));
-
+CREATE INDEX users_name_index ON users USING HASH (username);
+CREATE UNIQUE INDEX case_insensitive_name_index ON users USING HASH (lower(username));
+CREATE INDEX users_email_index ON users USING HASH (email);
 
 CREATE TABLE games (
     id BIGSERIAL PRIMARY KEY,
@@ -31,7 +31,7 @@ CREATE TABLE games (
     flags BIGINT NOT NULL DEFAULT 0
 );
 
-CREATE INDEX games_slug_index ON games(slug);
+CREATE INDEX games_slug_index ON games USING HASH (slug);
 
 CREATE TABLE categories (
     id BIGSERIAL PRIMARY KEY,
@@ -43,12 +43,16 @@ CREATE TABLE categories (
     flags BIGINT NOT NULL DEFAULT 0
 );
 
+CREATE INDEX categories_game_index ON categories USING HASH (game);
+
 CREATE TABLE permissions (
     user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     game_id BIGINT NOT NULL REFERENCES games(id) ON DELETE CASCADE,
     permissions BIGINT NOT NULL,
-    PRIMARY KEY (user_id, game_id)
+    PRIMARY KEY (game_id, user_id)
 );
+
+CREATE INDEX permissions_quick_lookup_index ON permissions USING HASH (user_id);
 
 CREATE TABLE runs (
     id BIGSERIAL PRIMARY KEY,
@@ -65,6 +69,11 @@ CREATE TABLE runs (
     verified_at TIMESTAMP,
     flags BIGINT NOT NULL DEFAULT 0
 );
+
+CREATE INDEX runs_category_index ON runs USING HASH (category);
+CREATE INDEX runs_submitter_index ON runs USING HASH (submitter);
+CREATE INDEX runs_score_index ON runs (score);
+CREATE INDEX runs_time_index ON runs (time);
 
 CREATE TABLE forum_entries (
     id BIGSERIAL PRIMARY KEY,
@@ -83,3 +92,7 @@ CREATE TABLE forum_entries (
     )
 );
 
+CREATE INDEX forum_comment_parent_index ON forum_entries USING HASH (parent);
+CREATE INDEX forum_post_game_index ON forum_entries USING HASH (game);
+CREATE INDEX forum_by_user_lookup_index ON forum_entries USING HASH (author);
+CREATE INDEX forum_time_sort_index ON forum_entries (created_at);
