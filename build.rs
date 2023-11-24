d349d1af3affb0ceaa5343d852e90a62175f63d2
valkyrie_pilot <valk@randomairborne.dev>
2023-11-23 17:43:40 -1000

    Try to fix CI

--- build.rs
+++ build.rs
@@ -3,6 +3,9 @@ fn main() {
         .args(["rev-parse", "--short", "HEAD"])
         .output()
         .expect("No git command found, or other git error!");
+    if !output.status.success() {
+        panic!("Bad exit status: {}", output.status)
+    }
     let git_output = String::from_utf8(output.stdout).unwrap();
     let git_hash = git_output.trim();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
